98836f8782df5190dc4c23f8346e6f86a25ded55
Lucas Rausch <lucas.rausch.732@k12.friscoisd.org>
2024-04-01 15:17:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Being Gay","Bako is a furry!!!"];
 
 
 let currentIndex = 0;
